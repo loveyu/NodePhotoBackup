b7eaa9bf212ca88cbba21d53c7539f2cadd8069a
loveyu <admin@loveyu.info>
2015-07-13 11:19:37 +0800

    添加MongoDB的操作

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var rc = require('./read_content.js');
+var mongo = require('./action_mongo.js');
 
+var startTime = new Date().getTime();
 rc(require("./config.js"), function (rc) {
+    var endTime = new Date().getTime();
+
+    console.error("Directory read time: " + ((endTime - startTime) / 1000));
+
     console.log(JSON.stringify(rc, null, "\t"));
+
+    mongo("photo_backup", function (db) {
+        db.insert(rc, function (result) {
+            //console.log(result);
+            console.error("Insert complete");
+            process.exit(0);
+        });
+    });
+
 });
--- read_content.js
+++ read_content.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var util = require('util');
+var md5 = require('MD5');
 
 /**
  * 读取文件系统
@@ -34,43 +36,42 @@ module.exports = function (config, finish) {
      * @param data 当前路径信息
      * @param name 当前路径名称
      * @param stat 路径的统计信息
-     * @param root_index 路径名称列表
+     * @param new_path 新的完整路径
      */
-    var set_data_info = function (data, name, stat, root_index) {
-        if (!data.hasOwnProperty(name)) {
+    var set_data_info = function (data, name, stat, new_path) {
+        var _name = md5(name);
+        if (!data.hasOwnProperty(_name)) {
+            data[_name] = {
+                'name': name,
+                'mtime': stat.mtime.getTime(),
+                'birthtime': stat.birthtime.getTime(),
+                'atime': stat.atime.getTime(),
+                'ctime': stat.ctime.getTime()
+            };
             if (stat.isDirectory()) {
-                data[name] = {
-                    'mtime': stat.mtime.getTime(),
-                    'ctime': stat.ctime.getTime(),
+                data[_name] = util._extend(data[_name], {
                     'dir_count': 0,
                     'file_count': 0,
                     'size_count': 0,
                     'size_count_h': "",
+                    'type': 'dir',
                     'dir': {}
-                };
+                });
             } else if (stat.isFile()) {
-                data[name] = {
-                    'mtime': stat.mtime.getTime(),
-                    'ctime': stat.ctime.getTime(),
+                data[_name] = util._extend(data[_name], {
                     'size': stat.size,
+                    'type': 'file',
                     'size_h': ""
-                };
-            }
-        }
-        var now_data = root_data;
-        if (stat.isFile()) {
-            ++now_data.file_count;
-            now_data.size_count += stat.size;
-        } else if (stat.isDirectory()) {
-            ++now_data.dir_count;
-        }
-        for (var i = 0; i < root_index.length; i++) {
-            now_data = now_data.dir[root_index[i]];
-            if (stat.isFile()) {
-                ++now_data.file_count;
-                now_data.size_count += stat.size;
-            } else if (stat.isDirectory()) {
-                ++now_data.dir_count;
+                });
+            } else if (stat.isSymbolicLink()) {
+                data[_name] = util._extend(data[_name], {
+                    'type': 'link',
+                    'link': fs.readlinkSync(new_path)
+                });
+            } else {
+                data[_name] = util._extend(data[_name], {
+                    'type': 'unknown'
+                });
             }
         }
     };
@@ -89,10 +90,17 @@ module.exports = function (config, finish) {
         return (Math.round(size * 100) / 100) + a[pos];
     };
 
+    /**
+     * 设置用户可读的文件大小
+     */
     var set_human_size = function () {
         root_data.size_count_h = format_size(root_data.size_count);
         set_human_size_re(root_data.dir);
     };
+    /**
+     * 开始递归计算
+     * @param data
+     */
     var set_human_size_re = function (data) {
         for (var i in data) {
             if (data.hasOwnProperty(i)) {
@@ -100,43 +108,84 @@ module.exports = function (config, finish) {
                     data[i].size_count_h = format_size(data[i].size_count);
                     set_human_size_re(data[i].dir);
                 } else {
-                    data[i].size_h = format_size(data[i].size);
+                    if (data[i].hasOwnProperty('size_h')) {
+                        data[i].size_h = format_size(data[i].size);
+                    }
                 }
             }
         }
+    };
+
+    var set_count_info = function (data, cb) {
+        var re = function (data) {
+            var s_c = 0;
+            var d_c = 0;
+            var f_c = 0;
+            for (var name in data.dir) {
+                if (data.dir.hasOwnProperty(name)) {
+                    if (data.dir[name].type === "file") {
+                        s_c += data.dir[name].size;
+                        ++f_c;
+                    } else if (data.dir[name].type === "dir") {
+                        re(data.dir[name]);//递归计算
+                        s_c += data.dir[name].size_count;
+                        d_c += (data.dir[name].dir_count + 1);
+                        f_c += data.dir[name].file_count;
+                    } else {
+                        //链接或其他数据计算为文件,不计大小
+                        ++f_c;
+                    }
+                }
+            }
+            data.size_count = s_c;
+            data.dir_count = d_c;
+            data.file_count = f_c;
+        };
+        if (data.hasOwnProperty('dir')) {
+            re(data);
+        }
+        cb();
     };
 
     /**
      * 读取路径
      * @param {object} data 当前目录的数据
      * @param {string} path 当前搜索的路径
-     * @param {Array} root_index 当前路径相对根目录的数组文件名称
-     * @param {string|null} next_index 下一个索引
      */
-    var read_dir = function (data, path, root_index, next_index) {
+    var read_dir = function (data, path) {
         ++read_record;
-        if (typeof next_index === "string") {
-            root_index.push(next_index);
-        }
         var call = function (err, list) {
             if (err) {
                 return;
             }
             for (var i = 0; i < list.length; ++i) {
                 var new_path = path + "/" + list[i];
-                var stat = fs.statSync(new_path);
-                set_data_info(data, list[i], stat, root_index);
+                var stat = null;
+                try {
+                    stat = fs.lstatSync(new_path);
+                    set_data_info(data, list[i], stat, new_path);
+                } catch (ex) {
+                    console.error(ex);
+                    continue;
+                }
+                if (stat.isSymbolicLink()) {
+                    //跳过链接文件
+                    continue;
+                }
                 if (stat.isDirectory()) {
-                    read_dir(data[list[i]].dir, new_path, array_clone(root_index), list[i]);
+                    //路径由MD5存储
+                    read_dir(data[md5(list[i])].dir, new_path);
                 }
             }
             --read_record;
             if (read_record === 0 && typeof finish === "function") {
-                set_human_size();
-                finish(root_data);
+                set_count_info(root_data, function () {
+                    set_human_size();
+                    finish(root_data);
+                });
             }
         };
         fs.readdir(path, call);
     };
-    read_dir(root_data.dir, root_path, [], null);
+    read_dir(root_data.dir, root_path);
 };
