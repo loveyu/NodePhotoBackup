79ccf0c6d1a2a565aab18014f8d788c5853e3fbf
loveyu <admin@loveyu.info>
2015-07-17 23:32:22 +0800

    更新

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
     path: "F:\\相片",//要检查的文件夹
-    backupPath: "F:\\bk",//备份到的目录文件夹
+    backupPath: "F:\\bk2",//备份到的目录文件夹
     backupConfig: "config.json",//备份目录的配置文件
+    backupDataInfo: "data.json",//备份目录的文件信息
     encode: "GB2312",//备份文件夹
     miniDept: 2,//最小压缩深度
     maxDir: 2 * 1024 * 1024 * 1024,//最大文件夹大小
---
+++ file_node.js
@@ -0,0 +1,12 @@
+var rc = require('./read_content.js');
+var fs = require('fs');
+var config = require("./config.js");
+
+var startTime = new Date().getTime();
+rc(config, function (rc) {
+    var endTime = new Date().getTime();
+
+    console.log("Directory read time: " + ((endTime - startTime) / 1000));
+
+    fs.writeFileSync(config.backupPath + "/" + config.backupDataInfo, JSON.stringify(rc, null, "\t"));
+});
